fd2796a6e2f369d085b2bb2ffec793525e91028e
Zoe Kirsh <zoekirsh@gmail.com>
2021-01-13 15:53:18 -0800

    stretch goal: delete button

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
-// document.addEventListener("DOMContentLoaded", () => {
-  
-// });
-
-//event handler 
+document.addEventListener("DOMContentLoaded", () => {
+//event handlers
 function addTaskToList (t) {
   let ul = document.querySelector("div#list ul");
   let li = document.createElement('li')
-  li.textContent = t.description;
+  const deleteBtn = document.createElement('button');
+
+  li.textContent = t
+  deleteBtn.textContent = 'done'
+
   ul.appendChild(li);
+
+  deleteBtn.addEventListener("click", handleDelete);
+  li.appendChild(deleteBtn);
+
 };
 
 function handleSubmit(e) {
   e.preventDefault();
-  let task = {"description": e.target.newTask.value};
-  addTaskToList(task);
+  let task = e.target["new-task-description"].value;
+  addTaskToList(task)
+  e.target.reset()
 };
 
-//event listener
-function addListeners() {
-  let form = document.querySelector("#create-task-form")
-  form.addEventListener("submit", handleSubmit);
+function handleDelete(e) {
+  e.target.parentElement.remove();
 };
 
-addListeners();
+//event listeners
+document.querySelector("#create-task-form").addEventListener("submit", handleSubmit);
+
+});
+
