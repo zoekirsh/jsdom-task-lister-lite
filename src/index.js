2da5d72a8c3b7a781081cb1500328d268b4ca749
Zoe Kirsh <zoekirsh@gmail.com>
2021-01-13 11:49:12 -0800

    MVP

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+  
+// });
+
+//event handler 
+function addTaskToList (t) {
+  let ul = document.querySelector("div#list ul");
+  let li = document.createElement('li')
+  li.textContent = t.description;
+  ul.appendChild(li);
+};
+
+function handleSubmit(e) {
+  e.preventDefault();
+  let task = {"description": e.target.newTask.value};
+  addTaskToList(task);
+};
+
+//event listener
+function addListeners() {
+  let form = document.querySelector("#create-task-form")
+  form.addEventListener("submit", handleSubmit);
+};
+
+addListeners();
